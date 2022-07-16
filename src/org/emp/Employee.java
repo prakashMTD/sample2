7a43ee13fc90a5b4dd8420c8167a3575f8eba3f9
prakashMTD <prakashmt95@gmail.com>
2022-07-16 19:39:52 +0530

    second push master

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,53 @@
+package org.emp;
+
+public class Employee {
+
+	
+	private void method1() {
+		System.out.println("Type 11");
+
+	}
+	
+	private void method2() {
+		System.out.println("Type 22");
+
+	}
+	
+	private void method3() {
+		System.out.println("Type 33");
+
+	}
+	private void method4() {
+		System.out.println("Type 44");
+
+	}
+	private void method5() {
+		System.out.println("Type 55");
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
